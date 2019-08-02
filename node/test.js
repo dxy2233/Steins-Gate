f97cc691c55740d935fdfd195d0554a343052eb3
dxy2233 <691369338@qq.com>
2019-08-02 17:18:55 +0800

    save

--- node/test.js
+++ node/test.js
@@ -5,26 +5,26 @@ const superagent = require('superagent') // ajax
 const fs = require("fs")
 const XLSX = require('xlsx') // excel
 const async = require('async')
+const voice = require('./voice')
 
 const cook ='global_cookie=ds7jcrh8va0pmwoji6yy4c6zw1gju13qzge; newhouse_user_guid=DD60A92F-13EF-BE67-E53A-FA6B941A06A5; vh_newhouse=1_1554289355_1010%5B%3A%7C%40%7C%3A%5D953cf1adb4deabbf8e0f358b09238859; Integrateactivity=notincludemc; lastscanpage=1; _sf_group_flag=xf; new_search_uid=ec665c5ecf71bf30320b9afc2aeeabe6; city=cq; newhouse_chat_guid=7E064831-319E-8B77-32DE-51761A28E999; Captcha=484D4F4855783766525663772F684F454E2F4F46336A474D346B51663961594E443636453035383668686F424A4B4F6E7669354C58594C754775666D71796370434D6370353539726633733D; __utma=147393320.996047671.1556542547.1563799566.1564407521.17; __utmc=147393320; __utmz=147393320.1564407521.17.14.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; unique_cookie=U_lddoxvvc5s9ldomzfwbhcucy71zjyofu9ps*1; route=4b4f06a73088080a32aa3eda08d524b8; sfut=347731973629C3497FB08F4C916CFB1BDC1189679A11D16DA66EEA7A9B66598A3297BC266718E66769403C4058736811CA75BC4FEE7564B377CABCE6126FC723467B3DEC0A488717364B3A99E20B8022FA117969E262C1D6F5863C238736A056; sfyt=2D9-olGHEtawNH-E-P93cJrrrJZnU3RJ1XwSse5YZN_YDxkF9zJrbvYFH6dhSzFQF2GHRe01aggWMzXs0THd2w==; unique_cookie=U_lddoxvvc5s9ldomzfwbhcucy71zjyofu9ps*2; sourcepage=wszx_gwgl_kfypc%7Cb_bmgl%5Elb_kfypc'
-
 superagent
-.get('https://m.fang.com/house/ec/customer/_AjaxCustomerList')
-.query({
-  NewCode: 3110122580,
-  PageSize: 10,
-  PageIndex: 1,
-  CallStatus: '',
-  CustomerNameOrPhone: '',
-  SalerNameOrPhone: '',
-  CreateTimeFrom: '开始时间',
-  CreateTimeTo: '结束时间'
-})
-.set('cookie', cook)
-.then(async res => {
+  .get('https://m.fang.com/house/ec/customer/_AjaxCustomerList')
+  .query({
+    NewCode: 3110122580,
+    PageSize: 30,
+    PageIndex: 1,
+    CallStatus: '',
+    CustomerNameOrPhone: '',
+    SalerNameOrPhone: '',
+    CreateTimeFrom: '开始时间',
+    CreateTimeTo: '结束时间'
+  })
+  .set('cookie', cook)
+  .then(async res => {
     let $ = cheerio.load(res.text)
     let arrayData = [
-      ['用户编号', '购房者姓名', '购房者手机号', '置业顾问', '通话状态', '客户产生时间', '客户来源', '操作', '音频地址', '通话时间', '通话时长',  '跟进记录', '意向', '通话记录']
+      ['用户编号', '购房者姓名', '购房者手机号', '置业顾问', '通话状态', '客户产生时间', '客户来源', '操作', '音频地址', '通话时长',  '跟进记录', '意向', '通话记录']
     ]
 
     // 获取主列表
@@ -61,21 +61,29 @@ superagent
         .set('cookie', cook)
         .then(res => {
           let $ = cheerio.load(res.text)
+          arrayData[i].splice(8, 1) // 删除详情页地址
+          let url = {} // mp3地址
+          let duration = [] // 通话时长
           $('.table tbody tr').each((k, elem) => {
-            arrayData[i].splice(8, 1)
-            arrayData[i].push(
-              {
-                v: '链接',
-                l: {
-                  Target: $(elem).children().eq(5).children().eq(0).attr('url'),
-                  Tooltip: $(elem).children().eq(5).children().eq(0).attr('url')
-                }
+            url = { ...url,
+              v: '链接',
+              l: {
+                Target: $(elem).children().eq(5).children().eq(0).attr('url'),
+                Tooltip: $(elem).children().eq(5).children().eq(0).attr('url')
               }
-            )
-            arrayData[i].push($(elem).children().eq(0).text())
-            arrayData[i].push($(elem).children().eq(2).text())
+            }
+            // url.push({
+            //   v: '链接',
+            //   l: {
+            //     Target: $(elem).children().eq(5).children().eq(0).attr('url'),
+            //     Tooltip: $(elem).children().eq(5).children().eq(0).attr('url')
+            //   }
+            // })
+            duration.push($(elem).children().eq(2).text())
           })
-          let remake = ''
+          arrayData[i].push(url)
+          arrayData[i].push(duration.join())
+          let remake = '' // 跟进记录
           $('.tab-content .new-timeline ul li').each((j, elem) => {
             remake = remake + $(elem).children().eq(2).text()
           })
@@ -84,6 +92,7 @@ superagent
     }
 
     // 输出excel
+    console.log(arrayData)
     let arrayWorkSheet = XLSX.utils.aoa_to_sheet(arrayData)
     let cc = {
       SheetNames: ['arrayWorkSheet'],
--- node/voice.js
+++ node/voice.js
@@ -22,6 +22,7 @@ const getId = async () => {
     .send({ file_len: fileInfo.size })
     .send({ file_name: '20190801100538970716822236.mp3' })
     .send({ slice_num: 1 })
+    .send({ has_seperate: true }) // 转写结果中是否包含发音人分离信息
     .then(res => {
       temp = JSON.parse(res.text).data
     })
@@ -31,24 +32,82 @@ const getId = async () => {
 // 上传
 const upload = async (id) => {
   await superagent
+    // .post('http://192.168.0.108:8082/excel/import')
     .post('https://raasr.xfyun.cn/api/upload')
     .set('Content-Type', 'multipart/form-data')
+    // .set('Authorization', 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU2NTI1MDAxOSwiaWF0IjoxNTY0NjQ1MjE5fQ.WLI66uhPWx89ScO6eJJA1vT-hbj3K3U2Dgg6GvNikdiHgw-SIQunn2rs902ajunew621kyyv-XuvGjIOx8LgQw')
     .field('app_id', appid)
     .field('signa', signa)
     .field('ts', ts)
     .field('task_id', id)
-    .field('slice_id', 'aaaaaaaaa')
+    .field('slice_id', 'aaaaaaaaaa')
     .attach('content', './audio/20190801100538970716822236.mp3')
     .then(res => {
-      console.log(res)
+      console.log(res.text + '上传')
     })
 }
 
-const start = async () => {
-  const taskId = await getId()
-  console.log(taskId)
-  await upload(taskId)
+// 合并
+const merge = async (id) => {
+  await superagent
+    .post('https://raasr.xfyun.cn/api/merge')
+    .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+    .send({ app_id: appid })
+    .send({ signa: signa })
+    .send({ ts: ts })
+    .send({ task_id: id })
+    .then(res => {
+      console.log(res.text + '合并')
+    })
 }
 
-start()
+// 查询进度
+const getProgress = async (id) => {
+  let temp
+  await superagent
+    .post('https://raasr.xfyun.cn/api/getProgress')
+    .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+    .send({ app_id: appid })
+    .send({ signa: signa })
+    .send({ ts: ts })
+    .send({ task_id: id })
+    .then(res => {
+      console.log(res.text + '查询')
+      temp = JSON.parse(res.text).data.status
+    })
+  return temp
+}
 
+// 获取结果
+const getResult = async (id) => {
+  let temp
+  await superagent
+    .post('https://raasr.xfyun.cn/api/getResult')
+    .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+    .send({ app_id: appid })
+    .send({ signa: signa })
+    .send({ ts: ts })
+    .send({ task_id: id })
+    .then(res => {
+      temp = JSON.parse(res.text).data
+    })
+  return temp
+}
+
+const start = async () => {
+  // const taskId = await getId()
+  // console.log(taskId)
+  // await upload(taskId)
+  // await merge(taskId)
+  // await getProgress('979742eec92c4d6dad79e99d47945445')
+  const res = await getResult('979742eec92c4d6dad79e99d47945445')
+  let text = ''
+  JSON.parse(res).map(item => {
+    text = text + (item.bg / 1000) + ':' + item.onebest + '\n'
+  })
+  console.log(text)
+}
+
+module.exports = {
+  start
+}
